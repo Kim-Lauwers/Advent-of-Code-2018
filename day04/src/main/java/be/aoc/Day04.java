533881cb056925238a38f6758e84e2f298632a78
Kim Lauwers <lauwers.kim@gmail.com>
2018-12-03 20:03:52 +0100

    Getting ready for day 4 !

---
+++ day04/src/main/java/be/aoc/Day04.java
@@ -0,0 +1,20 @@
+package be.aoc;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.nio.file.Files.lines;
+import static java.util.stream.Collectors.toList;
+
+public class Day04 {
+
+    public static void main(String[] args) throws Exception {
+        final Path path = Paths.get(Day04.class.getClassLoader().getResource("input.txt").toURI());
+
+        final List<String> lines = lines(path)
+                .map(String::new)
+                .collect(toList());
+
+    }
+}
